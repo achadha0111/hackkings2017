eee83d45a0f0dd2f305ab6152bfdfad1c881f12e
Prajna <prajnabhatt05@github.com>
2017-11-25 22:47:48 +0000

    Added functions to update db
    Functions added to connect to db and update

---
+++ function_driver.js
@@ -0,0 +1,9 @@
+const driver_form = function(full name,vehicle type,vehicle capacity,phone number)
+{driverRef.set({
+    full name: "hihi singh ",
+    vehicle type: "bullock cart",
+    vehicle capacity: "3",
+    phone number: "9833384731",
+  },
+);}
+module.exports = driver_form
