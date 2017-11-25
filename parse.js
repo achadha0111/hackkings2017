00c8cf358a4882f6d7e54c6e1c77e37ee02847ba
mihnea.savu@gmail.com <mihnea.savu@student.manchester.ac.uk.com>
2017-11-25 23:06:46 +0000

    Add input example to parse

--- parse.js
+++ parse.js
@@ -1,3 +1,25 @@
+/*
+input looks like this:
+{ ToCountry: 'GB',
+  ToState: 'Llanelli',
+  SmsMessageSid: 'SM0ac6f085e1ec57f5ca03e44bd274b1d3',
+   NumMedia: '0',
+   ToCity: '',
+   FromZip: '',
+   SmsSid: 'SM0ac6f085e1ec57f5ca03e44bd274b1d3',
+   FromState: '',
+   SmsStatus: 'received',
+   FromCity: '',
+   Body: 'Please please please ',
+   FromCountry: 'GB',
+   To: '+441554260044',
+   ToZip: '',
+   NumSegments: '1',
+   MessageSid: 'SM0ac6f085e1ec57f5ca03e44bd274b1d3',
+   AccountSid: 'ACf710fb920ebfe1e478f4e2a2531067d2',
+   From: '+447599938785',
+   ApiVersion: '2010-04-01' }
+*/
 const parse = (message) =>
 {
   return message.Body;
