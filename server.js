df3291f92f9766ccdc41a2ce3045d809e82d9318
Mihnea Savu <mihnea.savu@student.manchester.ac.uk.com>
2017-11-25 20:09:57 +0000

    Add get to server

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.post('/sms', function(req, res) {
   res.end(twiml.toString());
 });
 
+app.get('/sms', function(req, res) {
+  var twilio = require('twilio');
+  var twiml = new twilio.TwimlResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
 http.createServer(app).listen(process.env.PORT, function () {
   console.log("Express server listening on port 1337");
 });
