768ce9a7a563994df284c6dac600f8e0e363f064
mihnea.savu@gmail.com <mihnea.savu@student.manchester.ac.uk.com>
2017-11-26 05:30:36 +0000

    Small changed to parse

--- parse.js
+++ parse.js
@@ -103,8 +103,9 @@ return new Promise(function(res, err) {
 
           }
           else if ((userMessage.indexOf("Have") > -1) && (userMessage.indexOf("seats") > -1)) {
+            res("ok");
              updateRiderRequestTable(userMessage[2], userMessage[4], userMessage[6], Body.from);
-             res("ok");
+
            }
 
           // The user is present and hence the message is
